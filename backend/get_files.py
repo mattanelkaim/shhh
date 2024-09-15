5fc0aad00c78a320f159f21c7f9bac1c5da4234a
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-09-15 14:14:37 +0300

    added plan for backend

---
+++ backend/get_files.py
@@ -0,0 +1,8 @@
+import requests
+
+# We will use the Github's trees API
+# https://api.github.com/repos/mitre/cti/git/trees/master
+# enterprise-attack: https://api.github.com/repos/mitre/cti/git/trees/abc7cc6ab8cd685b3cf73ae19d835f9828cec6bd
+# attack-pattern: https://api.github.com/repos/mitre/cti/git/trees/73b36ee6886d871f354c113983860255841430d7
+
+
