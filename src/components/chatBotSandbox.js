682a27789a703aa480da0dae2fb156adf773485a
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-10-02 16:00:11 +0300

    analysis is working!

--- src/components/chatBotSandbox.js
+++ src/components/chatBotSandbox.js
@@ -40,11 +40,18 @@ export const VirusTotalAnalyzer = () => {
                 }
             );
 
-            const reportURL = response.data.permalink;
+            console.log('response:');
+            console.log(response);
+            console.log('data:');
+            console.log(response.data);
+            console.log('data.data:');
+            console.log(response.data.data);
+            
+            const reportURL = response.data.data.links.self;
 
             // Get report results (in a JSON format)
             const analysisResponse = await axios.get(
-                `https://www.virustotal.com/api/v3/files/${reportURL}`,
+                reportURL,
                 {
                 headers: {
                     'x-apikey': VirusTotalAPIKey,
@@ -81,3 +88,34 @@ export const VirusTotalAnalyzer = () => {
         </div>
     );
 };
+
+/*
+{
+  "data": {
+    "id": "N2YzMjQ4ZjllNzA4MzI4NGI1MzVlMWI0OWMzZGNlMWM6MTcyNzg3MjYzMg==",
+    "type": "analysis",
+    "links": {
+      "self": "https://www.virustotal.com/api/v3/analyses/N2YzMjQ4ZjllNzA4MzI4NGI1MzVlMWI0OWMzZGNlMWM6MTcyNzg3MjYzMg==",
+      "item": "https://www.virustotal.com/api/v3/files/e387106952b5db40cfb0a4d088f73a7707583b17e5fe2e86066150121d0fd14a"
+    },
+    "attributes": {
+      "stats": {
+        "malicious": 0,
+        "suspicious": 0,
+        "undetected": 61,
+        "harmless": 0,
+        "timeout": 0,
+        "confirmed-timeout": 0,
+        "failure": 0,
+        "type-unsupported": 15
+      },
+      "results": {
+        "Bkav": {
+          "method": "blacklist",
+          "engine_name": "Bkav",
+          "engine_version": "2.0.0.1",
+          "engine_update": "20241002",
+          "category": "undetected",
+          "result": null
+        },
+*/
