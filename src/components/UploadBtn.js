450b4f76d9a8e4f12b79a30783011dd2ccc6a6dc
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-10-03 10:51:15 +0300

    started to move functionality to server

--- src/components/UploadBtn.js
+++ src/components/UploadBtn.js
@@ -3,57 +3,33 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { FaFileUpload } from "react-icons/fa";
 
-const VirusTotalAPIKey = '';
-
 export const UploadBtn = () => {
-    const [results, setResults] = useState(null);
     const [loading, setLoading] = useState(false); // Used for styling
-    const [error, setError] = useState(null);
 
     const handleFileChange = async (event) => {
         const file = event.target.files[0];
-
+        
         if (!file)
             return;
 
         setLoading(true);
-        setError(null);
 
         try {
-            // Create a formData object in order to send the file
             const formData = new FormData();
             formData.append('file', file);
-
-            // Send the file to the sandbox
-            const response = await axios.post(
-                'https://www.virustotal.com/api/v3/files',
-                formData, // Send the file
-                {
+            console.log(formData);
+        
+            const response = await axios.post('http://localhost:3001/api/analyze', formData, {
                 headers: {
-                    'x-apikey': VirusTotalAPIKey,
-                    'Content-Type': 'multipart/form-data',
+                  'Content-Type': 'multipart/form-data', // No longer needed in frontend
                 },
-                }
-            );
-
-            const reportURL = response.data.permalink;
-
-            // Get report results (in a JSON format)
-            const analysisResponse = await axios.get(
-                `https://www.virustotal.com/api/v3/files/${reportURL}`,
-                {
-                headers: {
-                    'x-apikey': VirusTotalAPIKey,
-                },
-                }
-            );
-            setResults(analysisResponse.data);
+              });
+        
+            // Handle response from the backend
+            console.log(response.data);
         } catch (error) {
+            // Handle errors
             console.error(error);
-            if (error.status === 401) // Unauthorized
-                setError('Internal server ERROR with API key.');
-            else
-                setError(error.message);
         } finally {
             setLoading(false);
         }
